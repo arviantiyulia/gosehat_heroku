855c00353d3671dd1f4d3b6d7c23b8deb77d0153
Arvianti <arviantiyulia16@gmail.com>
2018-11-26 19:46:46 +0700

    commit app

--- app.py
+++ app.py
@@ -324,10 +324,10 @@ def handle_text_message(event):
             salam = "Selamat Pagi "
         elif dt.datetime.now() > dt.datetime.now().replace(hour=12, minute=0, second=0) and dt.datetime.now() < dt.datetime.now().replace(hour=17, minute=0, second=0):
             salam = "Selamat Siang "
-        elif dt.datetime.now() > dt.datetime.now().replace(hour=18, minute=0, second=0):
+        elif dt.datetime.now() > dt.datetime.now().replace(hour=18, minute=0, second=0) and dt.datetime.now() < dt.datetime.now().replace(hour=0, minute=0, second=0):
             salam = "selamat Malam "
         else:
-            salam = "Assalamualaikum"
+            salam = "Assalamualaik"
 
         msg_penyakit = "Anda terkena penyakit "
         msg_pengobatan = "Pengobatan yang harus dilakukan adalah "
