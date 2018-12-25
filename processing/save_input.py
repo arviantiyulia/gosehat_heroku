2eef7565b8f0b73d99322a6c07b8f3808ce68b17
Arvianti <arviantiyulia16@gmail.com>
2018-12-25 21:46:23 +0700

    Simpan database input user dan set treshold penyakit

--- processing/save_input.py
+++ processing/save_input.py
@@ -3,6 +3,8 @@ gejala = []
 def save_input(user_id, name_user, inputs, conn):
     cursor = conn.cursor()
 
+    print(user_id + " " + name_user)
+
     for i in range(len(inputs)):
         cursor.execute("INSERT INTO gejala_input(user_id, nama_user, nama_gejala) VALUES('"+str(user_id)+"', '"+name_user+"','"+inputs[i]+"')")
         conn.commit()
