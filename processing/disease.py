ca84633b424ad5195ac47a1d656e054a594060e5
Arvianti <arviantiyulia16@gmail.com>
2019-01-08 14:56:03 +0700

    benerin debug

--- processing/disease.py
+++ processing/disease.py
@@ -48,11 +48,12 @@ def get_disease(conn, cf, id):
 
 
     # --- HANYA UNTUK TUJUAN DEBUG ---
-    print("\nHasil Certainty Factor: ")
-    for item in id_new:
-        cursor.execute("SELECT * FROM penyakit WHERE id_penyakit = " + str(item))
-        disease_name = cursor.fetchall()
-        print("ID: ", item[0], " Nama: ", disease_name[0][1], " CF: ", item[1])
+    # print("\nHasil Certainty Factor: ")
+    # for item in cf_list:
+    #     cursor.execute("SELECT * FROM penyakit WHERE id_penyakit = " + str(item[0]))
+    #     disease_name = cursor.fetchall()
+    #     print("ID: ", item[0], " Nama: ", disease_name[0][1], " CF: ", item[1])
+    print("cf_list = ", cf_list)
 
     # print(disease_name)
     # print(disease_name[0][0][1])
