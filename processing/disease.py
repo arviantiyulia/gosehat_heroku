596423637a8a5f5f0082585e1b5bf5c0872d605f
Arvianti <arviantiyulia16@gmail.com>
2019-01-08 15:05:08 +0700

    benerin debug

--- processing/disease.py
+++ processing/disease.py
@@ -50,7 +50,7 @@ def get_disease(conn, cf, id):
     # --- HANYA UNTUK TUJUAN DEBUG ---
     print("\nHasil Certainty Factor: ")
     for item in cf_list:
-        cursor.execute("SELECT * FROM penyakit WHERE id_penyakit = " + str(item[0][0]))
+        cursor.execute("SELECT * FROM penyakit WHERE id_penyakit = " + item[0][0])
         disease_name = cursor.fetchall()
         print("ID: ", item[0], " Nama: ", disease_name[0][1], " CF: ", item[0][1])
     # print("cf_list = ", cf_list[0][0])
@@ -58,7 +58,7 @@ def get_disease(conn, cf, id):
     # print(disease_name)
     # print(disease_name[0][0][1])
     # HANYA UNTUK TUJUAN DEBUG
-    # print("\nKemungkinan penyakit yang diderita: " + str(disease_name[0][1]))
+    print("\nKemungkinan penyakit yang diderita: " + str(disease_name[0][1]))
     # print("Dengan nilai certainty factor: ", max_cf)
 
     return disease_name
