96b989f2a3d49421c904903706b9d1af040a6fe4
Arvianti <arviantiyulia16@gmail.com>
2019-01-11 22:11:36 +0700

    bot pindah ke fungsi

--- app.py
+++ app.py
@@ -362,7 +362,7 @@ def handle_text_message(event):
             cursor.execute("SELECT status FROM menu WHERE id_user LIKE '%" + user_id + "%'")
             count_menu = cursor.fetchall()
 
-        print("count menu = ", count_menu[0][0])
+        print("count menu = ", count_menu)
 
         if count_menu[0][0] == '\informasi':
             # print("informasi")
