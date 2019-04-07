ff992dab0e718c114915a4d7258da5139c44af7b
Arvianti <arviantiyulia16@gmail.com>
2019-04-07 21:38:06 +0700

    tambah tentang aplikasi

--- app.py
+++ app.py
@@ -568,7 +568,7 @@ def message_bot(user_id, name_user, salam, text, time, conn):
                           + "\n\n" + msg_peringatan
 
                 output_sistem = msg_penyakit + result[0][0][1] + " , " + result[1][0][1] + " , " + result[2][0][1]
-                disease_id = result[0][0][0] + "," + result[1][0][0] + "," + result[2][0][0]
+                disease_id = str(result[0][0][0]) + "," + str(result[1][0][0]) + "," + str(result[2][0][0])
                 save_history(user_id, name_user, text, output_sistem, disease_id, time, conn)
                 # save_history(user_id, name_user, text, output_sistem, conn)
 
