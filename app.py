529d9832617476332f63154faf2e6bb90122c34c
Arvianti <arviantiyulia16@gmail.com>
2019-04-11 17:13:34 +0700

    benerin rule

--- app.py
+++ app.py
@@ -539,7 +539,7 @@ def message_bot(user_id, name_user, salam, text, time, conn):
     elif kondisi_gejala == "kurang":
 
         print("input save = ", symp_db)
-        print("input 0 = ", symp_db[0][1])
+        print("input 0 = ", symp_db[0][0][1])
         # TODO: masukin gejala ke database, panggil fungsi bantuan
         # save_input(user_id, name_user, symp_db, conn)
 
